8c0149341a3edeebb3ea08ce409967e6dd2583d7
Rudra Patel <rudrapatel762006@gmail.com>
2026-07-01 19:59:36 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (72.80%) - LeetSync

--- 1585-the-kth-factor-of-n/the-kth-factor-of-n.cpp
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int kthFactor(int n, int k) {
-        vector<int> fact;
         for(int i=1;i<=n;i++){
             if(n%i==0){
-                fact.push_back(i);
+                k--;
+                if(k==0){
+                    return i;
+                }
             }
         }
 
-        if(fact.size()<k){
-            return -1;
-        }
-
-        int ans = fact[k-1];
-        return ans;
+        return -1;
     }
 };
