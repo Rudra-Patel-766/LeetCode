a545f8215e6a70668f030e854956d47f95cdafea
Rudra Patel <rudrapatel762006@gmail.com>
2026-09-15 17:33:40 +0530

    Time: 6 ms (13.26%) | Memory: 25.3 MB (5.81%) - LeetSync

---
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        unordered_map<int,int> mpp;
+        for(auto it:nums){
+            mpp[it]++;
+        }
+
+        sort(nums.begin(),nums.end());
+        int i=k;
+        while(true){
+            if(i%k==0 && mpp[i]==0){
+                break;
+            }
+            i++;
+        }
+
+        return i;
+    }
+};
